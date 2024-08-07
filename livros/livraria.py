d3168ad7987ae28d560c4f427608e590d4f47f91
Deleon <delps.santos1987@gmail.com>
2024-08-06 21:26:34 -0300

    123

--- livros/livraria.py
+++ livros/livraria.py
@@ -1,19 +1,47 @@
 import sqlite3 as bd
 from datetime import date
-lista=[]
-#conectar com um banco de dados com o nome "livraria"
-conexao= bd.connect("livraria")
-#o cursor recebe a conexao para manipular o bd
-cursor= conexao.cursor()
 
-cursor.execute("""create table if not exists livros
-               (
-               id_livro integer primary key, 
-               nome_livro text, 
-               preco_livro real, 
-               ano_livro text
-               )
-               """)
+lista=[]
+
+
+def gravar():
+    cursor,conexao=conectar()
+    cursor.execute("""
+            create table if not exists livro(
+                id_livro integer primary key autoincrement, 
+                nome_livro text, 
+                preco_livro real, 
+                ano_livro text
+            )
+    """)
+    
+    cursor.execute(" insert into livro (nome_livro, preco_livro, ano_livro) VALUES(?,?,?)" ,(lista[0], lista[1], lista[2]))
+    
+    conexao.commit()
+    cursor.execute(" select * from livro")
+    print("ID               Livro                       Preço        Ano")
+    for row in cursor.fetchall():
+        linha =row
+        if linha:
+            
+            print(f'{linha[0]}   {linha[1]} {linha[2]} {linha[3]}')
+            
+        
+    print("gravados")
+    cursor.close()
+    conexao.close()
+
+
+
+def conectar():
+
+    #conectar com um banco de dados com o nome "livraria"
+    conex= bd.connect("livraria")
+    #o cursor recebe a conexao para manipular o bd
+    curs= conex.cursor()
+    return curs,conex
+
+    
 while True:
     while True:
         nome=input("Qual livro deseja adicionar? ")
@@ -34,13 +62,16 @@ while True:
             continue
     while True:
         ano=input("Informe o ano de lançamento do Livro: ") 
+        print()
         if ano:
-            lista.insert(2,ano) 
-            break    
+            lista.insert(2,ano)
+            gravar() 
+            break   
+
         else:
             print("Informe o ano")
             continue
-    finalize=input("""'S'-sair ou 'C'-continuar""")
+    finalize=input("""\n'S'-sair ou 'C'-continuar""")
     finalize=finalize.upper()[0]
     if finalize in "SC":
         if finalize== "S":
